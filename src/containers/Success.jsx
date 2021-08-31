dd4fa45c2f5c2a673159143ba3f497597125802f
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-31 14:49:53 -0500

    Integrate API googleMaps

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import AppContext from '../context/AppContext'
+import Map from '../components/Map'
 import '../styles/components/Success.css'
 
-const Success = () => (
+const Success = () => {
+  const { state } = useContext(AppContext);
+  const { buyer} = state;
+
+  return(
   <div className="Success">
     <div className="Success-content">
-      <h2>Nombre, Gracias por tu compra</h2>
+      <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
       <span>Tu pedido llegara en 3 dias a tu dirección:</span>
       <div className="Success-map">
-        Google Maps
+        <Map/>
       </div>
     </div>
   </div>
   )
-
+}
 export default Success
